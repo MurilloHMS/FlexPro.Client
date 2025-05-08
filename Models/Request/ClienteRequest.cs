a7dc51c2274f6d9cdffddbaa02e83359e8fed0a1
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-08 12:37:16 -0300

    feat: migrado clientes para nova arquitetura
    criado request e response para controlar cliente

---
+++ Models/Request/ClienteRequest.cs
@@ -0,0 +1,9 @@
+namespace FlexPro.Client.Models.Request;
+
+public class ClienteRequest
+{
+    public string CodigoSistema { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Status { get; set; }
+}
---
+++ Models/Response/ClienteResponse.cs
@@ -0,0 +1,10 @@
+namespace FlexPro.Client.Models.Response;
+
+public class ClienteResponse
+{
+    public int Id { get; set; }
+    public string CodigoSistema { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Status { get; set; }
+}
