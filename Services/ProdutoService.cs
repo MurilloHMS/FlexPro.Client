ab76b6656136eccb2b137ec6c2180f0448c425f7
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-06-03 11:10:08 -0300

    feat: criado rota para consulta única do produto

--- Services/ProdutoService.cs
+++ Services/ProdutoService.cs
@@ -19,4 +19,10 @@ public class ProdutoService
         var stream = await _http.GetStreamAsync("api/produto/produtoloja");
         return await JsonSerializer.DeserializeAsync<List<ProdutoLojaResponse>>(stream, _options);
     }
+
+    public async Task<ProdutoLojaResponse> GetByIdAsync(int id)
+    {
+        var stream = await _http.GetStreamAsync($"api/produto/produtoloja/{id}");
+        return await JsonSerializer.DeserializeAsync<ProdutoLojaResponse>(stream, _options);
+    }
 }
