0154e0cf4cf6f04fa4aadf4f5806a658e8aba4c7
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-07-15 08:34:48 -0300

    feat: melhorado componente do processo de consultoria

---
+++ wwwroot/js/animations.js
@@ -0,0 +1,13 @@
+window.initializeStepsObserver = () => {
+    const steps = document.querySelectorAll('.step');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = '1';
+                entry.target.style.transform = 'translateY(0)';
+            }
+        });
+    }, { threshold: 0.2 });
+
+    steps.forEach(step => observer.observe(step));
+}
