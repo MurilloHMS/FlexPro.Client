1af2e03e5d1d304f28685d25d557786acaddc5f0
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-04 09:16:50 -0300

    refactor(contact): enhanced VehicleService constructor and method return handling

--- Infrastructure/Services/VehicleService.cs
+++ Infrastructure/Services/VehicleService.cs
@@ -5,38 +5,29 @@ using FlexPro.Client.Infrastructure.Interfaces;
 
 namespace FlexPro.Client.Infrastructure.Services;
 
-public class VehicleService : IVehicleService
+public class VehicleService(HttpClient http) : IVehicleService
 {
-    private readonly HttpClient _client;
 
-    public VehicleService(HttpClient client)
-    {
-        _client = client;
-    }
+    private static readonly string[] VehicleBrands =
+    [
+        "ACURA", "ALFA ROMEO", "AUDI", "BMW", "BUICK", "CADILLAC", "CHEVROLET", "CHERY", "CITROËN", "DODGE", "FERRARI",
+        "FIAT", "FORD", "GMC", "HONDA",
+        "HYUNDAI", "INFINITI", "JAGUAR", "JEEP", "KIA", "LAMBORGHINI", "LAND ROVER", "LEXUS", "LINCOLN", "MITSUBISHI",
+        "MERCEDES-BENZ",
+        "MINI", "MITSUBISHI", "NISSAN", "PEUGEOT", "PORSCHE", "RAM", "RENAULT", "SANTA FÉ", "SEAT", "SUBARU", "SUZUKI",
+        "TESLA", "TOYOTA", "VOLKSWAGEN",
+        "VOLVO", "ZAZ", "TROLLER", "JAC MOTORS", "FOTON", "GREAT WALL", "TATA", "BYD", "DIESEL", "MERCURY", "SPYKER",
+        "SMART", "DAEWOO", "FIAT CHRYSLER",
+        "HONDA", "LIFAN", "CHERY", "CROSSFOX", "RENAULT", "FORD"
+    ];
 
-    public string[] GetVehicleBrands()
-    {
-        string[] marcas =
-        {
-            "ACURA", "ALFA ROMEO", "AUDI", "BMW", "BUICK", "CADILLAC", "CHEVROLET",
-            "CHERY", "CITROËN", "DODGE", "FERRARI", "FIAT", "FORD", "GMC", "HONDA",
-            "HYUNDAI", "INFINITI", "JAGUAR", "JEEP", "KIA", "LAMBORGHINI",
-            "LAND ROVER", "LEXUS", "LINCOLN", "MITSUBISHI", "MERCEDES-BENZ",
-            "MINI", "MITSUBISHI", "NISSAN", "PEUGEOT", "PORSCHE", "RAM", "RENAULT",
-            "SANTA FÉ", "SEAT", "SUBARU", "SUZUKI", "TESLA", "TOYOTA", "VOLKSWAGEN",
-            "VOLVO", "ZAZ", "TROLLER", "JAC MOTORS", "FOTON", "GREAT WALL", "TATA",
-            "BYD", "DIESEL", "MERCURY", "SPYKER", "SMART", "DAEWOO", "FIAT CHRYSLER",
-            "HONDA", "LIFAN", "CHERY", "CROSSFOX", "RENAULT", "FORD"
-        };
-
-        return marcas;
-    }
+    public IReadOnlyList<string> GetVehicleBrands() => VehicleBrands;
 
     public async Task<ApiResponse<List<Veiculo>>> GetVehicleAsync()
     {
         try
         {
-            var response = await _client.GetAsync("api/veiculo");
+            var response = await http.GetAsync("api/veiculo");
 
             var statusCode = response.StatusCode;
             var rawMessage = await response.Content.ReadAsStringAsync();
